eefe99b3dcd2fcd1962dd7be396289d346ecb364
yutuno <yuto_uno51@yahoo.co.jp>
2019-05-06 16:27:05 +0900

    Added: Script

---
+++ install-theme.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+destination="$HOME/Library/Developer/Xcode/UserData/FontAndColorThemes"
+
+if [[ ! -d $destination ]]; then
+    mkdir -p ${destination}
+fi
+
+find . -name "*.xccolortheme" -exec cp {} ${destination} \;
+
+echo "Copied to: ${destination} 📝"
